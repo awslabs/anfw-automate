2249bc4edb74d820056271a5b1a478e0d27d3134
Ajinkya Patil <ajinkya.secure@gmail.com>
2024-03-15 17:02:06 +0100

    log handler tests

--- app/tests/test_log_handler.py
+++ app/tests/test_log_handler.py
@@ -1,14 +1,19 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from datetime import datetime
-from log_handler import CustomerLogHandler, Level
+from datetime import datetime, timedelta
+from lib.log_handler import CustomerLogHandler, Level
 
 
 class TestCustomerLogHandler(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+
+    # Set up the test case with mock credentials and log group name
+    @patch("lib.log_handler.boto3.client")
+    def setUp(self, mock_boto3_client):
+        # Mock the boto3 client
+        mock_boto3_client.return_value = MagicMock()
+
         # Initialize the CustomerLogHandler with mock credentials
-        cls.handler = CustomerLogHandler(
+        self.handler = CustomerLogHandler(
             log_group_name="test-log-group",
             credentials={
                 "AccessKeyId": "mock_access_key",
@@ -17,31 +22,17 @@ class TestCustomerLogHandler(unittest.TestCase):
             },
         )
 
-    @patch("log_handler.boto3.client")
-    def test_init(self, mock_boto3_client):
-        # Test that the constructor sets up the necessary attributes
-        handler = CustomerLogHandler(
-            log_group_name="test-log-group",
-            credentials={
-                "AccessKeyId": "mock_access_key",
-                "SecretAccessKey": "mock_secret_key",  # nosec: Not a real key
-                "SessionToken": "mock_session_token",
-            },
-        )
+    def test_init(self):
+        self.assertEqual(self.handler.log_group_name, "test-log-group")
+        self.assertIsNotNone(self.handler.logger)
+        self.assertIsNotNone(self.handler.s3client)
+        self.assertIsNotNone(self.handler.logclient)
 
-        self.assertEqual(handler.log_group_name, "test-log-group")
-        self.assertIsNotNone(handler.logger)
-        self.assertIsNone(handler.version)
+    def test_update_version(self):
+        self.handler.update_version("1.0.0")
+        self.assertEqual(self.handler.version, "1.0.0")
 
-        mock_boto3_client.assert_called_with(
-            "logs",
-            aws_access_key_id="mock_access_key",
-            aws_secret_access_key="mock_secret_key",  # nosec: Not a real key
-            aws_session_token="mock_session_token",
-        )
-
-    @patch("log_handler.boto3.client")
-    def test_send_log_message(self, mock_boto3_client):
+    def test_send_log_message(self):
         # Test sending log message
         log_stream_name = "test-log-stream"
         message = "Test message"
@@ -49,7 +40,7 @@ class TestCustomerLogHandler(unittest.TestCase):
 
         # Mock the put_log_events method
         mock_log_events = MagicMock()
-        mock_boto3_client.return_value.put_log_events = mock_log_events
+        self.handler.logclient.put_log_events = mock_log_events
 
         # Mock the _check_log_stream method
         mock_check_log_stream = MagicMock()
@@ -65,7 +56,6 @@ class TestCustomerLogHandler(unittest.TestCase):
 
         # Assertions
         mock_check_log_stream.assert_called_with(log_stream_name)
-        mock_generate_time_stamp.assert_called_once()
 
         mock_log_events.assert_called_with(
             logGroupName="test-log-group",
@@ -78,7 +68,39 @@ class TestCustomerLogHandler(unittest.TestCase):
             ],
         )
 
-    # Add more tests for other methods as needed...
+    def test_export_logs_to_s3(self):
+        log_stream_name = "test-log-stream"
+        bucket_name = "XXXXXXXXXXX"
+
+        # Mock the put_log_events method
+        mock_log_events = MagicMock()
+        self.handler.logclient.create_export_task = mock_log_events
+
+        # Mock dates
+        datetoday = int(round(datetime.now().timestamp() * 1000))
+        start_date = datetime.now() - timedelta(29)
+        date29daysback = int(round(start_date.timestamp() * 1000))
+
+        self.handler.export_logs_to_s3(log_stream_name, bucket_name)
+
+        mock_log_events.assert_called_with(
+            taskName="NFW_Customer_Log_Export",
+            logGroupName="test-log-group",
+            logStreamNamePrefix=log_stream_name,
+            fromTime=date29daysback,
+            to=datetoday,
+            destination=bucket_name,
+            destinationPrefix=f"{log_stream_name}",
+        )
+
+    def test_generate_log_stream_name(self):
+        datetoday_var = int(round(datetime.now().timestamp() * 1000))
+        log_stream_name_var = datetime.now().strftime("%Y/%m/%d/%H/%M")
+
+        self.assertEqual(
+            self.handler.generate_log_stream_name(),
+            f"{log_stream_name_var}/{datetoday_var}",
+        )
 
 
 if __name__ == "__main__":
